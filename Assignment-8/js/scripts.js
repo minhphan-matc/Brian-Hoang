0834a575836e200108b33a23d6368c049d1e6072
Brian <hoangbt@gmatc.matc.edu>
2018-04-16 09:58:10 -0500

    Assignment-8

--- Assignment-8/js/scripts.js
+++ Assignment-8/js/scripts.js
@@ -35,6 +35,7 @@ var $nav = $('#nav-container');
 var $intro = $('#intro');
 var $posts = $('#post-container');
 
+
 function initPosts(){
   for(var i = 0; i < data.length; i++){
 
@@ -76,5 +77,55 @@ function initPosts(){
     $intro.fadeIn(1000);
   }
 }
+function addPost(event){
+  var inputTitle = document.getElementById("title").value;
+  var inputEl = document.getElementById("input-quote").value;
+  var inputAuth = document.getElementById("author").value;
+  if(inputEl.value !== ""){
+    var postId = 'post-' + (data.length + 1),
+    $post = $('<section class="post"></section>'),
+    $title = $('<h2 class="title"></h2>'),
+    $body = $('<blockquote></blockquote>'),
+    $author = $('<span class="author"></span>'),
+    $navItem = $('<li></li>');
 
-initPosts();
+    $title.text(inputTitle);
+    $body.text(inputEl);
+    $author.text(inputAuth);
+    //add nav item data
+    $navItem.attr('id', (data.length + 1));
+    $navItem.text(inputTitle);
+
+    //Combine post elements
+    $post.attr('id', postId);
+    $post.append($title);
+    $post.append($body);
+    $post.append($author);
+
+    //add post and nav elements to page
+    $posts.append($post);
+    $nav.append($navItem);
+
+    //wire up nav item click event
+    $navItem.on('click', function(){
+      var id = $(this).attr('id');
+      $posts.children().hide();
+      $posts.find('#post-' + id).fadeIn();
+    });
+
+    //hide all post and show the intro
+    $posts.children('.post').hide();
+    $intro.fadeIn(1000);
+
+    //reset input
+    inputTitle.value = "Title";
+    inputEl.value = "Quote";
+    inputAuth.value = "Author";
+  }
+}
+function init(){
+  initPosts();
+  document.getElementById("add-task").onclick = addPost;
+}
+
+init();
